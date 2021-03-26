98df43fa3ff0f6ee4934a45b8d286a2ed4378431
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-03-26 14:44:30 +0100

    Search input added to make the search dynamic

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import axios from 'axios'
 
 export default function App() {
   const [results, setResults] = useState([])
+  const [query, setQuery] = useState('reacthooks')
 
   useEffect(() => {
     getResults()
-    }, [])
+    }, [query])
     
   const getResults = async () => {
     const response = await axios.get(
-      'http://hn.algolia.com/api/v1/search?query=reacthooks'
+      `http://hn.algolia.com/api/v1/search?query=${query}`
     )
     setResults(response.data.hits)
   }
 
   return (
     <>
+      <input type="text" onChange={event => setQuery(event.target.value)}/>
       <ul>
         {results.map(result => (
           <li key={result.objectID}>
