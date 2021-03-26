0f6d3b7cb42f00133e5370abed9e7fed34b2c6e7
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-03-26 14:30:26 +0100

    Adding a useEffect hook to handle the call to the API. Also testing it with the async wait function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function App() {
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    getResults()
+    }, [])
+    
+  const getResults = async () => {
+    const response = await axios.get(
+      'http://hn.algolia.com/api/v1/search?query=reacthooks'
+    )
+    setResults(response.data.hits)
+  }
+
   return (
-    <div>
-      App
-    </div>
+    <>
+      <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
